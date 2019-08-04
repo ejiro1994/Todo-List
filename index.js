48da8136bd378460087ae1947a0bcc34f5628459
Ejiro Lennox Akpoduado <ejiro1994@gmail.com>
2019-08-04 22:37:41 +0100

    second commit - button styling add todo

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-let d =  new Date()
+//today's date 
+let d = new Date()
 document.getElementById('date').innerHTML = d.toDateString()
+
+//todo logic
+let add_todo_button = document.getElementById('add-todo')
+
+
+let input_box = document.getElementById('input')
+
+let createTask = () => {
+
+
+	let todo = `<div></div>`
+	todo.className = 'todo-item'
+
+
+	let node = document.getElementById('todos-container').appendChild(todo)
+
+	
+
+}
+
+
+add_todo_button.addEventListener('click', function () {
+	createTask()
+})
